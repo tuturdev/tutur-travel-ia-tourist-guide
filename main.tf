516b05fa45e7c31426cdda54e48f798c94bd97b4
Andrés Rivadeneira <avrz237@gmail.com>
2024-09-23 20:33:15 -0500

    update role

--- main.tf
+++ main.tf
@@ -29,23 +29,6 @@ resource "aws_s3_bucket_acl" "json_bucket_acl" {
   count  = length(try([data.aws_s3_bucket.existing_bucket.id], [])) == 0 ? 1 : 0
 }
 
-# Crear un Lambda para manejar la API
-resource "aws_lambda_function" "tutur_lambda" {
-  filename         = "lambda_function.zip"
-  function_name    = "TuturRAGLambda"
-  role             = aws_iam_role.lambda_exec[0].arn  # Acceso a la instancia indexada
-  handler          = "lambda_function.lambda_handler"
-  runtime          = "python3.8"
-  # Puedes omitir source_code_hash temporalmente para evitar el error
-  # source_code_hash = filebase64sha256("lambda_function.zip")
-}
-
-# API Gateway para exponer el endpoint
-resource "aws_api_gateway_rest_api" "tutur_api" {
-  name        = "Tutur API"
-  description = "API para gestionar los paseos de Tutur"
-}
-
 # Verificar si el rol IAM "tutur_lambda_execution_role" ya existe
 data "aws_iam_role" "existing_lambda_role" {
   name = "tutur_lambda_execution_role"
@@ -71,12 +54,26 @@ resource "aws_iam_role" "lambda_exec" {
   })
 }
 
+# Crear un Lambda para manejar la API
+resource "aws_lambda_function" "tutur_lambda" {
+  filename         = "lambda_function.zip"
+  function_name    = "TuturRAGLambda"
+
+  # Utilizamos una expresión condicional para elegir el ARN correcto
+  role = length(aws_iam_role.lambda_exec) > 0 ? aws_iam_role.lambda_exec[0].arn : data.aws_iam_role.existing_lambda_role.arn
+
+  handler          = "lambda_function.lambda_handler"
+  runtime          = "python3.8"
+  # Puedes omitir source_code_hash temporalmente para evitar el error
+  # source_code_hash = filebase64sha256("lambda_function.zip")
+}
+
 # Política de IAM (solo si se creó el rol)
 resource "aws_iam_role_policy" "lambda_exec_policy" {
-  count = length(aws_iam_role.lambda_exec)  # Uso de length() en lugar de count
+  count = length(aws_iam_role.lambda_exec)
 
   name   = "tutur_lambda_policy"
-  role   = aws_iam_role.lambda_exec[0].name  # Acceso a la instancia indexada
+  role   = aws_iam_role.lambda_exec[0].name
   policy = jsonencode({
     Version = "2012-10-17",
     Statement = [
