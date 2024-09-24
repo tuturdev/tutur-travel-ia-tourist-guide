eac6970188a89d3dc104f4569fdd6daad62999c3
Andrés Rivadeneira <avrz237@gmail.com>
2024-09-23 20:22:15 -0500

    add iam role validation

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ resource "random_id" "bucket_id" {
 resource "aws_s3_bucket" "json_bucket" {
   bucket = "0002-tutur-resources-${random_id.bucket_id.hex}"
 
-  # Crear el bucket solo si el bloque de datos "existing_bucket" falla
+  # Crear el bucket solo si el bloque de datos "existing_bucket" no encuentra el bucket
   count  = length(try([data.aws_s3_bucket.existing_bucket.id], [])) == 0 ? 1 : 0
 }
 
@@ -29,7 +29,6 @@ resource "aws_s3_bucket_acl" "json_bucket_acl" {
   count  = length(try([data.aws_s3_bucket.existing_bucket.id], [])) == 0 ? 1 : 0
 }
 
-
 # Crear un Lambda para manejar la API
 resource "aws_lambda_function" "tutur_lambda" {
   filename         = "lambda_function.zip"
@@ -41,25 +40,55 @@ resource "aws_lambda_function" "tutur_lambda" {
   # source_code_hash = filebase64sha256("lambda_function.zip")
 }
 
-
 # API Gateway para exponer el endpoint
 resource "aws_api_gateway_rest_api" "tutur_api" {
   name        = "Tutur API"
   description = "API para gestionar los paseos de Tutur"
 }
 
-# Definir IAM Role para Lambda
-resource "aws_iam_role" "lambda_exec" {
+# Verificar si el rol IAM "tutur_lambda_execution_role" ya existe
+data "aws_iam_role" "existing_lambda_role" {
   name = "tutur_lambda_execution_role"
+}
+
+# Crear el rol IAM solo si no existe
+resource "aws_iam_role" "lambda_exec" {
+  count = length(try([data.aws_iam_role.existing_lambda_role.id], [])) == 0 ? 1 : 0
+
+  name = "tutur_lambda_execution_role"
+
   assume_role_policy = jsonencode({
-    Version = "2012-10-17"
+    Version = "2012-10-17",
     Statement = [
       {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
         Principal = {
           Service = "lambda.amazonaws.com"
         }
+      }
+    ]
+  })
+}
+
+# Política de IAM (solo si se creó el rol)
+resource "aws_iam_role_policy" "lambda_exec_policy" {
+  count = aws_iam_role.lambda_exec.count
+
+  name   = "tutur_lambda_policy"
+  role   = aws_iam_role.lambda_exec[0].name
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "logs:*",
+        Effect = "Allow",
+        Resource = "*"
+      },
+      {
+        Action = "s3:*",
+        Effect = "Allow",
+        Resource = "*"
       }
     ]
   })
