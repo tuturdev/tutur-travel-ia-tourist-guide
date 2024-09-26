cc94df901203c9f95c8ad73f900bc871f286d6ce
Andrés Rivadeneira <avrz237@gmail.com>
2024-09-25 20:37:17 -0500

    increse timeout

--- modules/lambda/lambda.tf
+++ modules/lambda/lambda.tf
@@ -12,7 +12,7 @@ resource "aws_lambda_function" "tutur_lambda" {
   role          = aws_iam_role.lambda_exec[0].arn
   handler       = "lambda_function.lambda_handler"
   runtime       = "python3.12"
-  timeout       = 60  # Incrementa temporalmente a 60 segundos
+  timeout       = 200  # Incrementa temporalmente a 60 segundos
 
   # Sobrescribir el código cada vez
   source_code_hash = filebase64sha256("lambda_function.zip")
