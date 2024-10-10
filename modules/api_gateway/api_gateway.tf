3b72dcfa5f8a88debb469a476c70a60f230052a1
Andrés Rivadeneira <avrz237@gmail.com>
2024-10-09 20:26:38 -0500

    reset project

--- modules/api_gateway/api_gateway.tf
+++
@@ -1,7 +0,0 @@
-# modules/api_gateway/api_gateway.tf
-
-# Crear el API Gateway para exponer el endpoint
-resource "aws_api_gateway_rest_api" "tutur_api" {
-  name        = "Tutur API"
-  description = "API para gestionar los paseos de Tutur"
-}
--- variables.tf
+++
@@ -1,9 +0,0 @@
-variable "region" {
-  description = "La región donde se desplegará la infraestructura"
-  default     = "us-east-1"
-}
-
-variable "s3_bucket_name" {
-  description = "Nombre del bucket S3 para almacenar los archivos JSON"
-  default     = "0002-tutur-resources"
-}
